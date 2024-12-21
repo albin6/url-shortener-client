2af563c763e86c38cbcf028a8a0d2b4fd24ade50
Albin Aji <albinpariyarathu@gmail.com>
2024-12-22 00:25:37 +0530

    api base url updated

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://url-shortener-server-indol.vercel.app/api/v_1",
+  baseURL: "https://url-shortener-server-amy4.onrender.com/api/v_1",
   timeout: 10000,
 });
